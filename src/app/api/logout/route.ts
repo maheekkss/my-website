c6b1d4518f1689bdb09743c000ff2efef93c7b04
maheekkss <maheswarawekarajendra@gmail.com>
2025-10-03 20:27:33 +0700

    Update route.ts

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -4,12 +4,16 @@ export async function POST() {
   const res = NextResponse.json({ message: "Logout berhasil" });
 
   // Hapus cookie token
-  res.cookies.set("token", "", {
+  res.cookies.set({
+    name: "token",
+    value: "",
+    path: "/",
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    path: "/",
+    sameSite: "strict",
     maxAge: 0, // expire langsung
   });
 
   return res;
 }
+
